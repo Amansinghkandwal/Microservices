c02249657494205f8faf35e434c669689a0d4f34
aman <cooddeex@gmail.com>
2026-04-03 17:48:16 +0530

    Accounts service

---
+++ src/main/java/com/designX/accounts/AccountsApplication.java
@@ -0,0 +1,38 @@
+package com.designX.accounts;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditawareImpl")
+@OpenAPIDefinition(
+		info=@Info(
+				title="Accounts microservice REST API Documentation",
+				description = "DesignXBank Accounts microservices REST API Documentation",
+				version="v1",
+				contact = @Contact(
+						name="Aman Singh Kandwal",
+						email="coodeex@gmail.com"
+				),
+				license = @License(
+						name="Apache 2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "DesignXBank Accounts microservice REST API Documentation",
+				url="http://localhost:8080/swagger-ui/index.html#/"
+		)
+)
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
